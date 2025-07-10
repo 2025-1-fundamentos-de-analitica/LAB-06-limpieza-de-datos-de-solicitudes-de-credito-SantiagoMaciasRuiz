efa7a79cc8c1178abc04b0e33950b2044637bc00
Santiago Macias Ruiz <70926070+SantiagoMaciasRuiz@users.noreply.github.com>
2025-07-10 15:54:18 -0500

    hecho

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,21 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
+import os
+import pandas as pd
+
+
+def clean_text(column):
+    return column.str.lower().str.replace("-", " ").str.replace("_", " ").str.strip()
+
+
+def dates(date_series):
+    dates = pd.to_datetime(date_series, dayfirst=True, errors="coerce")
+    dates = dates.fillna(
+        pd.to_datetime(date_series, format="%Y/%m/%d", errors="coerce")
+    )
+    return dates
+
 
 def pregunta_01():
     """
@@ -13,3 +28,45 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    input_path = "files/input/solicitudes_de_credito.csv"
+    output_path = "files/output/solicitudes_de_credito.csv"
+
+    df = pd.read_csv(input_path, sep=";", index_col=0)
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["barrio"] = df["barrio"].str.lower().str.replace("_", " ").str.replace("-", " ")
+    columns = [
+        "idea_negocio",
+        "línea_credito",
+    ]
+    for column in columns:
+        df[column] = clean_text(df[column])
+
+    df["monto_del_credito"] = (
+        df["monto_del_credito"]
+        .str.strip()
+        .str.replace("$", "")
+        .str.replace(",", "")
+        .str.replace(".00", "")
+        .astype(int)
+    )
+    df["fecha_de_beneficio"] = dates(df["fecha_de_beneficio"])
+
+    df.drop_duplicates(inplace=True)
+
+    if os.path.exists(output_path):
+        os.remove(output_path)
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, sep=";")
+
+    print(df["sexo"].value_counts())
+    print(df["barrio"].value_counts())
+
+
+pregunta_01()
